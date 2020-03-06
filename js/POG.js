fe4692eb0920777b43d2691b61fd960d15795715
Chris Gray <combly123@gmail.com>
2020-03-06 16:31:00 -0500

    Initial Commit
    Inital commit to this gem of a site

---
+++ js/POG.js
@@ -0,0 +1,65 @@
+function getJSONP(url,callback) 
+{
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('GET', url, true);
+    xmlhttp.onreadystatechange = function() 
+    {
+        if (xmlhttp.readyState == 4)
+        {
+            if(xmlhttp.status == 200) 
+            {
+                var obj = JSON.parse(xmlhttp.responseText);
+                callback(obj)
+            }
+        }
+    };
+    xmlhttp.send(null);
+}
+
+function display_image(img)
+{
+    var ratio = 5;
+    const init_h = 89;
+    const init_w = 62;
+    var elem = document.createElement("img");
+    elem.src = img;
+    elem.width = ratio * init_w;
+    elem.height = ratio * init_h;
+    document.getElementById("div_img").appendChild(elem);
+}
+
+function process_card(card)
+{
+    var img = card.card_images[0]["image_url"];
+    display_image(img);
+    console.log(img);
+}
+
+function remove_children(id)
+{
+    var children = document.getElementById(id).innerHTML = '';
+}
+
+function draw()
+{
+    // Clear the container of cards 
+    remove_children("div_img");
+
+    var url = "https://db.ygoprodeck.com/api/v6/randomcard.php";
+    var callback = process_card;
+
+    // get the cards
+    getJSONP(url,callback); // card 1 
+    getJSONP(url,callback); // card 2 
+
+    return 0;
+}
+
+function setup()
+{
+    document.getElementById("btn_draw").addEventListener("click", draw);
+}
+
+// ""Entry Point""
+setup();
+
